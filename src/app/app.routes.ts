9db00c4dcdd53c48dadda5eb92f8aaaa7c51972f
Pedro Romero <pedrojesusromeroortega@gmail.com>
2018-12-18 20:00:44 +0100

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { AccountEntryViewComponent } from './account-entry-view/account-entry-view.component';
-import { AccountEntryFormComponent } from './account-entry-form/account-entry-form.component';
-
-export const appRoutes: Routes = [
-    { path: 'accounts', component: AccountEntryViewComponent },
-    { path: 'accounts/new', component: AccountEntryFormComponent },
-    { path: '', redirectTo: '/accounts', pathMatch: 'full' },
-];
--- src/app/models/accountEntry.model.ts
+++
@@ -1,6 +0,0 @@
-export interface AccountEntry {
-  id: number;
-  date: Date;
-  concept: string;
-  amount: number;
-}
