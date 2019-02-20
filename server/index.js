55a4d9ad98c881ddf59e7ecebeb9628cf6c234b8
Pedro Romero <pedrojesusromeroortega@gmail.com>
2019-02-20 22:30:10 +0100

    fix deprecation warnings in mongoose

--- server/index.js
+++ server/index.js
@@ -10,8 +10,9 @@ const app = express();
 // Set up mongoose connection
 const mongoose = require('mongoose');
 const dev_db_url = 'mongodb://someuser:abcd1234@ds123619.mlab.com:23619/productstutorial'; //link from mlab
-// const mongoDB = (process.env.MONGODB_URI ? `${process.env.MONGODB_URI}/contaRudy` : dev_db_url);
-const mongoDB = (process.env.MONGODB_URI ? `${process.env.MONGODB_URI}` : dev_db_url);
+const mongoDB = (process.env.MONGODB_URI ? `${process.env.MONGODB_URI}/contaRudy` : dev_db_url);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
@@ -24,6 +25,5 @@ app.use('/api/accountEntries', accountEntry);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server is up and running on port numner ${port}`);
+  console.log(`Server is up and running on port numner ${port}`);
 });
-
